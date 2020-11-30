0c87fd4b1dc26364702dc56874fa28b5eb1345b7
akasha.nasir <akasha.nasir@ARP-288>
2020-11-30 18:08:41 +0500

    Crud React

--- src/Component/User/AddUser.js
+++ src/Component/User/AddUser.js
@@ -3,7 +3,7 @@ import react, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 
 
-const AddUser = (props) => {
+const AddUser = () => {
     let history = useHistory();
     const [user, setUser] = useState({
         name: "",
@@ -11,51 +11,103 @@ const AddUser = (props) => {
         email: "",
         phone: "",
         website: "",
-        company: null
+        company: {
+            name: "",
+            catchPhrase: "",
+            bs: ""
+        },
+        address: {
+            street: "",
+            suite: "",
+            city: "",
+            zipcode: "",
+            geo: {
+                lat: "",
+                lng: ""
+
+            }
+        }
     });
 
 
 
-    const [company, setCompany] =  useState({
-        
-            name:null,
-            catchPhrase: null,
-            bs:null,
-        
-})
- 
 
     const { id } = useParams();
-    const { name, username, email, phone, website } = user;
-    const { name:compName, bs, catchPhrase} = company;
+    const { name, username, email, phone, website,
+        company: { name: compName, catchPhrase, bs },
+        address: { street, suite, city, zipcode,
+            geo: { lat, lng } } } = user;
+
 
 
 
     const onInputChange = e => {
-        setUser({ ...user, [e.target.name] : e.target.value });  
 
-        setCompany({...company,[e.target.name] : e.target.value});
-        setUser({ ...user, ['company'] : company  })
-        console.log(user)
+        let attributeName = e.target.name;
 
+        if (attributeName === "name" || attributeName === "username" || attributeName === "email" || attributeName === "phone" || attributeName === "website") {
 
+            setUser({ ...user, [e.target.name]: e.target.value });
+
+        } else if (attributeName === "compName" || attributeName === "catchPhrase" || attributeName === "bs") {
+
+            setUser({ ...user, company: { ...user.company, [e.target.name]: e.target.value } });
+
+        } else if (attributeName === "street" || attributeName == "suite" || attributeName === "city" || attributeName === "zipcode") {
+
+            setUser({ ...user, address: { ...user.address, [e.target.name]: e.target.value } });
+
+        } else if (attributeName === "lat" || attributeName === "lng") {
+
+            //Maintain old User, state maintain address old state, maintain old geo state and set new value to geo
+            setUser({ ...user, address: { ...user.address, geo: { ...user.address.geo, [e.target.name]: e.target.value } } });
+
+        }
+        
     };
+   
 
-    const onSubmit = e => {
+
+    const onSubmit = async  e => {
         e.preventDefault();
         if (id) {
-            Axios.put(`http://localhost:3333/users/${id}`, user).then(res => {
-            console.log('Update');    
-            history.push("/");
+             Axios.put(`http://localhost:3333/users/${id}`, user).then(res => {
+                console.log('Update');
+                history.push("/");
             })
                 .catch(error => {
                     console.log("error {error}");
                 });
-        } else{
+        } else {
 
+            // setUser({
+            //     name: "akasha",
+            //     username: "akasha ali",
+            //     email: "akashaali2012@gmail.com",
+            //     phone: "03322659846",
+            //     website: "1234456",
+            //     company: {
+            //         name: "Arpatech",
+            //         catchPhrase: "CEG",
+            //         bs: "Dot Net dev"
+            //     },
+            //     address: {
+            //         street: "1-468 liaquatabad",
+            //         suite: "1",
+            //         city: "karachi",
+            //         zipcode: "79600",
+            //         geo: {
+            //             lat: "12.70",
+            //             lng: "18.90"
+            //         }
+            //     }
+            // });
+        
+            // console.log(user);
+            
             Axios.post('http://localhost:3333/users', user).then(res => {
-                console.log('Add');   
-            history.push("/");
+                console.log('Add');
+                history.push("/");
             })
                 .catch(error => {
                     console.log(`error ${error}`);
@@ -65,37 +117,55 @@ const AddUser = (props) => {
     };
 
     useEffect(() => {
-       onLoad();
-
+        onLoad(id);
+        
     }, [id]); //initialize empty [] it run only one time initialize with id [id] run when [id change]
 
 
-    const onLoad = () => {
+    const onLoad = (id) => {
         if (id) {
             Axios.get(`http://localhost:3333/users/${id}`).then(res => {
-            setUser(res.data);
-            setCompany(res.data.company)
-                
+                setUser(res.data);
+
+
             })
                 .catch(error => {
                     console.log("error {error}");
                 });
-        }else{
+        } else {
             setUser({
                 name: "",
                 username: "",
                 email: "",
                 phone: "",
-                website: ""
+                website: "",
+                company: {
+                    name: "",
+                    catchPhrase: "",
+                    bs: ""
+                },
+                address: {
+                    street: "",
+                    suite: "",
+                    city: "",
+                    zipcode: "",
+                    geo: {
+                        lat: "",
+                        lng: ""
+                    }
+                }
             });
+
         }
     };
+
+
    
-   
-    return (  
+    return (
         <div className="container border shadow">
             <h1 >{id ? 'Edit User' : 'Add User'}</h1>
-            <form onSubmit={e => onSubmit(e)}>
+            <form onSubmit={e =>  onSubmit(e)}>
+                <h1>User Info:</h1>
                 <div className="form-group">
                     <label htmlFor="name">Name</label>
                     <input
@@ -105,8 +175,8 @@ const AddUser = (props) => {
                         placeholder="Name"
                         name="name"
                         value={name}
-                        onChange={e => onInputChange(e)} 
-                        />
+                        onChange={e => onInputChange(e)}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="userName">User Name</label>
@@ -117,8 +187,8 @@ const AddUser = (props) => {
                         placeholder="User Name"
                         name="username"
                         value={username}
-                        onChange={e => onInputChange(e)} 
-                        />
+                        onChange={e => onInputChange(e)}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="email">Email address</label>
@@ -129,8 +199,8 @@ const AddUser = (props) => {
                         placeholder="Enter email"
                         name="email"
                         value={email}
-                        onChange={e => onInputChange(e)} 
-                        />
+                        onChange={e => onInputChange(e)}
+                    />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
@@ -141,8 +211,8 @@ const AddUser = (props) => {
                         placeholder="Phone Number"
                         name="phone"
                         value={phone}
-                        onChange={e => onInputChange(e)} 
-                        />
+                        onChange={e => onInputChange(e)}
+                    />
                 </div>
                 <div className="form-group">
                     <label htmlFor="website">Web Site</label>
@@ -152,21 +222,115 @@ const AddUser = (props) => {
                         placeholder="Website Name"
                         name="website"
                         value={website}
-                        onChange={e => onInputChange(e)} 
-                        />
-                </div> 
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <h1>Company Info:</h1>
+
                 <div className="form-group">
-                    <label htmlFor="website">Web Site</label>
+                    <label htmlFor="compName">company Name</label>
                     <input type="Text"
                         className="form-control"
-                        id="website"
-                        placeholder="Website Name"
+                        id="compName"
+                        placeholder="Company Name"
+                        name="compName"
+                        value={compName}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="bs">Bs</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="bs"
+                        placeholder="bs"
                         name="bs"
                         value={bs}
-                        onChange={e => onInputChange(e)} 
-                        />
-                </div> 
-                <button type="submit" className="btn btn-primary">{id ? 'Update User' : 'Add User'}</button>
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="catchphrase">Catch Phrase</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="catchphrase"
+                        placeholder="Catch Phrase"
+                        name="catchPhrase"
+                        value={catchPhrase}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <h1>Address Info:</h1>
+                <div className="form-group">
+                    <label htmlFor="street">Street</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="street"
+                        placeholder="Street"
+                        name="street"
+                        value={street}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="suite">suite</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="suite"
+                        placeholder="suite"
+                        name="suite"
+                        value={suite}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="city">City</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="city"
+                        placeholder="City"
+                        name="city"
+                        value={city}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="zipcode">Zip Code</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="zipcode"
+                        placeholder="Zip Code"
+                        name="zipcode"
+                        value={zipcode}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <h1>Geo Info:</h1>
+                <div className="form-group">
+                    <label htmlFor="lat">Latitude</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="lat"
+                        placeholder="Latitude"
+                        name="lat"
+                        value={lat}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="lng">Longitude</label>
+                    <input type="Text"
+                        className="form-control"
+                        id="lng"
+                        placeholder="Langitude"
+                        name="lng"
+                        value={lng}
+                        onChange={e => onInputChange(e)}
+                    />
+                </div>
+
+
+                <button type="submit" className="btn btn-primary" >{id ? 'Update User' : 'Add User'}</button>
             </form>
         </div>
     )
